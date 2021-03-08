8cdcc75d4adc28eb411096f7dcdd754eb0425544
Jbrown380 <77353425+Jbrown380@users.noreply.github.com>
2021-03-07 19:32:45 -0500

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,1 +1,17 @@
-document.querySelector("main")
+var main = document.querySelector(".main")
+
+var joke1 = "Why is Peter Pan always flying? He neverlands";
+var joke2 = "I'm so good at sleeping. I can do it with my eyes closed"
+var joke3 = "My boss told me to have a good day.. so I went home"
+var ".main"
+main = 
+<template>
+    <h1>"My Jokes"<h1>
+    var joke1;
+    var joke2;
+    var joke3;
+</template>
+
+var p = pdocument.createElement(".p") 
+var p = "That's all folks!"
+document.getElementById(".main").appendChild(p);
